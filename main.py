504b6a89096878973a4498793fd9eb44726d3896
GabPoulin <poulin33@me.com>
2024-03-01 00:51:01 -0500

    tests in main

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # test
 import general_design
 
-kd = general_design.kd("Continue")
-print(kd)
+kd = general_design.load_duration("continue", 1.5, 0.5)
+print(f"Kd = {kd}")
+
+b, d, phi, kh, kfi = general_design.FireResistance(
+    30, 140, 350, product="sciage"
+).effective_section()
+print(f"b = {b}")
+print(f"d = {d}")
+print(f"phi = {phi}")
+print(f"Kh = {kh}")
+print(f"Kfi = {kfi}")
