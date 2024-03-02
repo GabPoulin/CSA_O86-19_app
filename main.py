7033b3238e66a00289dc5a7a6a82da7309c05d17
GabPoulin <poulin33@me.com>
2024-03-02 13:06:05 -0500

    commit

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 import general_design
 
 kd = general_design.load_duration("continue", 1.5, 0.5)
-print(f"Kd = {kd}")
+print(f"{kd = }")
+print("")
 
 b, d, phi, kh, kfi = general_design.FireResistance(
-    30, 140, 350, product="sciage"
+    duration=30,
+    width=140,
+    depth=350,
+    product="sciage",
 ).effective_section()
-print(f"b = {b}")
-print(f"d = {d}")
-print(f"phi = {phi}")
-print(f"Kh = {kh}")
-print(f"Kfi = {kfi}")
+print(f"{b = }")
+print(f"{d = }")
+print(f"{phi = }")
+print(f"{kh = }")
+print(f"{kfi = }")
+print("")
