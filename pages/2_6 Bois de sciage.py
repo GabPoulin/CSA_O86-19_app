a6a51a46ff11b49984a09b8fed1cbdabf27ffe1a
GabPoulin <poulin33@me.com>
2025-10-28 21:00:18 -0400

    commit

--- pages/2_6 Bois de sciage.py
+++ pages/2_6 Bois de sciage.py
@@ -6,10 +6,19 @@ import sawn_lumber as saw
 st.title("6 | Bois de Sciage")
 st.page_link("Accueil.py", label="Retour à l'accueil")
 
-b = st.number_input("Largeur de l'élément (mm)", min_value=38)
-d = st.number_input("Hauteur de l'élément (mm)", min_value=38)
-msr = st.checkbox("Bois de charpente classé mécaniquement par résistance (MSR)")
-mel = st.checkbox("Bois de charpente évalué par machine (MEL)")
-categorie = saw.lumber_category(b, d, msr, mel)
 
-st.write(categorie)
+col1, col2, col3 = st.columns(3)
+b = col1.number_input("Largeur de l'élément (mm)", min_value=38, width=160)
+d = col2.number_input("Hauteur de l'élément (mm)", min_value=38, width=160, value=140)
+msr_mel = col3.pills("Bois classé mécaniquement?", options=("MSR", "MEL"))
+msr, mel = False, False
+if msr_mel == "MSR":
+    msr = True
+elif msr_mel == "MEL":
+    mel = True
+
+WOOD_CATEGORIE = saw.lumber_category(b, d, msr, mel)
+
+st.divider()
+st.metric("Catégorie:", value=WOOD_CATEGORIE)
+st.divider()
